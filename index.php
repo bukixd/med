3f79556fd348b417af5a9695c027eb07b11c70ea
bukixd <mieszczakkuba@gmail.com>
2022-03-23 11:04:19 +0100

    wyciaganie zbazy

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 
-echo "Hi";
+$db = new mysqli("localhost", "root", "", "med");
+
+$q = $db->prepare("SELECT * FROM staff");
+if($q->execute()) {
+    //jezeli dziala
+    $result = $q->get_result();
+    while($row = $result->fetch_assoc()) {
+        //lot?
+        $id = $row['id'];
+        $firstName = $row['firstName'];
+        $lastName = $row['lastName'];
+        echo "Lekarz $firstName $lastName<br>";
+    }
+} else {
+    //jezeli nie ladzia
+    die("nie ladzia");
+
+}
+
 
 
 ?>
