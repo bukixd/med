a350be00764bb923346f54b945eff43037175be5
bukixd <mieszczakkuba@gmail.com>
2022-04-13 11:17:38 +0200

    elo

---
+++ admin/addStaff.php
@@ -0,0 +1,24 @@
+<?php
+    if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
+
+        $db = new mysqli("localhost", "root", "", "med1");
+        $q = $db->prepare("INSERT INTO staff VALUES(NULL, ?, ?)");
+        $q->bind_param("ss", $_REQUEST['firstName'], $_REQUEST['lastName']);
+        if($q->execute()){
+            echo "Dodano Lekarza";
+        }
+
+    } else {
+        echo '        
+        <form action="addStaff.php" method="post">   
+        <label for="firstName">Imię</label>
+        <input type="text" name="firstName" id="firstName">
+        <label for="lastName">Nazwisko</label>
+        <input type="text" name="lastName" id="lastName">
+        <input type="submit" value="Dodaj nowy presonel">
+        </form>
+        ';
+
+    }
+?>        
+        
